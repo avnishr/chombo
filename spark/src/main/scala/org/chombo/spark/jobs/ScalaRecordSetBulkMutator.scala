3e69a80f32b8a866695def09f6dc760074d48a86
avi <avnishr@gmail.com>
2017-03-12 23:02:31 +0530

    Scala version of RecordSetBulkMutator

--- spark/src/main/scala/org/chombo/spark/jobs/ScalaRecordSetBulkMutator.scala
+++ spark/src/main/scala/org/chombo/spark/jobs/ScalaRecordSetBulkMutator.scala
@@ -1,7 +1,7 @@
 package org.chombo.spark.jobs
 
 import org.chombo.spark.common.JobConfiguration
-
+import org.apache.spark.SparkContext
 
 /**
   * Created by avi on 3/12/2017.
@@ -11,10 +11,22 @@ object ScalaRecordSetBulkMutator extends JobConfiguration {
   def main(args: Array[String]) {
 
     val appName = "recordSetBulkMutator"
+
     val Array(inputPath: String, outputPath: String, configFile: String) = getCommandLineArgs(args, 3)
 
-    println(inputPath)
-    println(outputPath)
+    val config = createConfig(configFile)
+
+    val sparkConf = createSparkConf(appName, config, false)
+    val sparkCntxt = new SparkContext(sparkConf)
+    val appConfig = config.getConfig(appName)
+
+    val invalidRecordsOutputFile = getOptionalStringParam(appConfig, "invalid.records.output.file")
+    val fieldDelimIn = getStringParamOrElse(appConfig, "field.delim.in", ",")
+    val fieldDelimOut = getStringParamOrElse(appConfig, "field.delim.out", ",")
+
+    println("The spark configuration for invalid records is " + invalidRecordsOutputFile)
+    println("The spark configuration for field delim in " + invalidRecordsOutputFile)
+    println("The spark configuration for field delim out " + invalidRecordsOutputFile)
 
 
   }
